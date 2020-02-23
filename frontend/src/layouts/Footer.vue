50e1c5b7b61f26f4ad6f45db6cad673627ae52df
prismathic <pjjsalam1999@gmail.com>
2020-02-23 16:20:24 +0100

    Added header and footer with bootstrap etal

---
+++ frontend/src/layouts/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer text-center">
+    <div class="container p-3">
+      <span class="text-muted">&copy; {{date}} <a class="specColor" href="http://www.mis.ui.edu.ng">Information Technology and Media Services</a> (ITeMS), University of Ibadan </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+    data () {
+        return {
+            date: new Date().getFullYear()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
