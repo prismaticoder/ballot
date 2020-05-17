782246d085b23e8ae0b59772c19eacfb51060324
prismathic <pjjsalam1999@gmail.com>
2020-05-17 13:55:42 +0100

    Fix: Modified error json format

--- backend/controllers/res.js
+++ backend/controllers/res.js
@@ -7,25 +7,31 @@ exports.sendRes = function(res,data,status=0) {
     return res.status(message.status).json(message);
 };
 
-exports.sendError = function(res,status) {
+exports.sendError = function(res,status,customMsg=undefined) {
     const message = {};
 
-    switch (status) {
-        case 404:
-            message.message = "Resource Not Found"
-            break;
-        case 500:
-            message.message = "Internal Server Error"
-            break;
-        case 400:
-            message.message = "Invalid Request"
-            break;
-        case 401:
-            message.message = "You are not authorized to make this request"
-            break;
-        case 403:
-            message.message = "Access Forbidden"
-            break;
+    if (typeof customMsg != 'undefined') {
+        message.error = customMsg
+    }
+
+    else {
+        switch (status) {
+            case 404:
+                message.error = "Resource Not Found"
+                break;
+            case 500:
+                message.error = "Internal Server Error"
+                break;
+            case 400:
+                message.error = "Invalid Request"
+                break;
+            case 401:
+                message.error = "You are not authorized to make this request"
+                break;
+            case 403:
+                message.error = "Access Forbidden"
+                break;
+        }
     }
 
     message.success = false;
