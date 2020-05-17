249dcc697abb54b1b4ae465e8e5ad95a4af66848
prismathic <pjjsalam1999@gmail.com>
2020-05-18 00:08:21 +0100

    Feat: updated the routes and set a requireAuth beforeEach for each route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,8 +3,13 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import AdminHome from '../views/admin/Home.vue'
 import Voters from '../views/admin/Voters.vue'
-import Candidates from '../views/admin/Candidates.vue'
+import Candidates from '../views/Candidates.vue'
+import Accreditation from '../views/Accreditation.vue'
+import SingleCandidate from '../views/SingleCandidate.vue'
+import AdminLogin from '../views/AdminLogin.vue'
+import AdminCandidates from '../views/admin/Candidates.vue'
 import Settings from '../views/admin/Settings.vue'
+import store from '../store'
 
 
 Vue.use(VueRouter)
@@ -24,31 +29,64 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
+    path: '/candidates',
+    name: 'all-candidates',
+    component: Candidates
+  },
+  {
+    path: '/candidates/:id',
+    name: 'single-candidate',
+    component: SingleCandidate
+  },
+  {
+    path: '/accreditation',
+    name: 'accreditation',
+    component: Accreditation
+  },
+  {
+    path: '/admin/login',
+    name: 'admin-login',
+    component: AdminLogin,
+  },
+  {
     path: '/admin',
     name: 'admin-home',
     component: AdminHome,
-    // children: [
-    //   {
-    //     // path: '/voters',
-    //     // name: 'voters',
-    //     // component: Voters
-    //   },
-    // ]
+    meta: {
+      requireAuth: true
+    }
   },
   {
     path: '/admin/voters',
     name: 'voters',
-    component: Voters
+    component: Voters,
+    meta: {
+      requireAuth: true
+    }
   },
   {
     path: '/admin/candidates',
     name: 'candidates',
-    component: Candidates
+    component: AdminCandidates,
+    meta: {
+      requireAuth: true
+    }
+  },
+  {
+    path: '/admin/candidates/:id',
+    name: 'candidates',
+    component: SingleCandidate,
+    meta: {
+      requireAuth: true
+    }
   },
   {
     path: '/admin/settings',
     name: 'settings',
-    component: Settings
+    component: Settings,
+    meta: {
+      requireAuth: true
+    }
   },
 
 
@@ -57,6 +95,22 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next({
+      path: '/admin/login',
+      nextUrl: to.fullPath
+    }) 
+  } else {
+    next() 
+  }
 })
 
 export default router
