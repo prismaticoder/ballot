40c45b13023a29dba56b66ec7363c89c21312a95
prismathic <pjjsalam1999@gmail.com>
2020-05-18 00:09:48 +0100

    Feat: Added interceptor for expired responses and states should be set before each view is created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <Header />
+    <div class="col-md-9 mx-auto text-center">
+      <router-view/>
     </div>
-    <router-view/>
+    
+    <Footer />
   </div>
 </template>
 
+<script>
+
+import Header from './layouts/Header.vue';
+import Footer from './layouts/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer
+  },
+  beforeCreate() {
+    this.$store.dispatch('setState')
+    .then(res => console.log("App state updated!"))
+    .catch(err => console.log(`Couldn't check app state: ${err}`))
+  },
+  created: function () {
+    this.$http.interceptors.response.use(undefined, function (err) {
+      return new Promise(function (resolve, reject) {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch('logout')
+          resolve()
+        }
+        reject(err);
+      });
+    });
+  }
+
+}
+</script>
+
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -28,5 +59,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
