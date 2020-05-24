fed92c4af23e864aa975e352098482ae1eb098f8
prismathic <pjjsalam1999@gmail.com>
2020-05-25 00:22:39 +0100

    Fix: adjusted format for sending tken

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,7 +15,7 @@ const token = localStorage.getItem('userToken')
 Vue.prototype.$http = axios;
 
 if (token) {
-  Vue.prototype.$http.defaults.headers.common['Authorization'] = token
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = `Bearer ${token}`
 }
 
 Vue.config.productionTip = false
