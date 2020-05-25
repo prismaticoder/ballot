6b67c202b43179b71f241dad148e4f6e9f56f026
prismathic <pjjsalam1999@gmail.com>
2020-05-25 22:50:46 +0100

    done with getting categories, now on to setting the election timing

--- frontend/src/components/admin/SingleCategory.vue
+++ frontend/src/components/admin/SingleCategory.vue
@@ -4,7 +4,7 @@
           <h5>POST</h5>
           <span class="text-capitalize">{{category.name}}</span>
       </div>
-      <div class="row border-bottom">
+      <div class="row">
         <div class="col-6 border-right">
             <h5>MIN. LEVEL</h5>
             {{category.minLevel}} Level
@@ -14,36 +14,54 @@
             {{category.maxLevel}} Level
         </div>
       </div>
-      <div class="col-12 justify-content-center">
+      <div class="col-12 border-top justify-content-center">
           <!-- Edit and Delete Buttons -->
             <v-btn fab small :color="btnColor" style="color: #162059" title="Edit Category Details" class="mr-3 btn-fix" @click.prevent="editMode = true">
                 <v-icon>mdi-pencil</v-icon>
             </v-btn>
-            <v-btn fab small :color="btnColor" style="color: #162059" title="Remove Category" dark class="ml-5 btn-fix" @click.prevent="$emit(deleteCategory, category.id)">
+            <v-btn fab small :color="btnColor" style="color: #162059" title="Remove Category" dark class="ml-5 btn-fix" @click.prevent="dialog = true">
                 <v-icon>mdi-delete</v-icon>
             </v-btn>
       </div>
+      <!-- Confirmation Dialog -->
+        <v-dialog v-model="dialog" persistent max-width="350">
+            <v-card>
+                <v-card-title class="headline">Cancel Application?</v-card-title>
+                <v-card-text>
+                Are you sure you want to delete this category: <strong>{{category.name}}</strong>?
+                Deleting this would also mean you are automatically cancelling the application of every candidate associated with this position.
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="green darken-1" text @click="dialog = false">No</v-btn>
+                    <v-btn color="green darken-1" text @click="$emit('deleteCategory', category.id)">Yes</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
   </div>
     <div class="border" v-else>
+        <b-alert class="col-12 mx-auto" v-model="showAlert" variant="warning" dismissible>
+            {{errorMsg}}
+        </b-alert>
       <div class="col-12 border-bottom">
           <h5>POST</h5>
-          <input type="text" class="form-control" v-model="category.name">
+          <input type="text" class="form-control" v-model="name">
       </div>
-      <div class="row border-bottom">
+      <div class="row">
         <div class="col-6 border-right">
             <h5>MIN. LEVEL</h5>
-            <b-form-select v-model="category.minLevel">
+            <b-form-select required v-model="minLevel">
                 <b-form-select-option :value="level" v-for="level in levels" :key="level">{{level}}</b-form-select-option>
             </b-form-select>
         </div>
         <div class="col-6 border-left">
             <h5>MAX. LEVEL</h5>
-            <b-form-select v-model="category.maxLevel">
+            <b-form-select required v-model="maxLevel">
                 <b-form-select-option :value="level" v-for="level in levels" :key="level.id">{{level}}</b-form-select-option>
             </b-form-select>
         </div>
       </div>
-      <div class="col-12 justify-content-center">
+      <div class="col-12 border-top justify-content-center">
         <v-btn fab small :color="btnColor" dark style="color: #162059" title="Cancel" class="mr-3 btn-fix" @click.prevent="editMode = false">
             <v-icon>mdi-close</v-icon>
         </v-btn>
@@ -62,21 +80,43 @@ export default {
         return {
             btnColor: "#fff",
             editMode: false,
+            showAlert: false,
+            errorMsg: null,
+            dialog: false,
+            name: this.category.name,
+            minLevel: this.category.minLevel,
+            maxLevel: this.category.maxLevel,
+            id: this.category.id
         }
     },
     methods: {
         updateCategory() {
-            let { name, minLevel, maxLevel,id } = this.category;
-            this.$http.put(`${process.env.VUE_APP_URL}/admin/categories/${id}`, {
-                name,minLevel,maxLevel
-            })
-            .then(res => {
+            let { name, minLevel, maxLevel,id } = this;
+            name = name.replace(/\s+/g,' ').trim()
+
+            //check if there was any change before going to the API
+            if (name == this.category.name && minLevel == this.category.minLevel && maxLevel == this.category.maxLevel) {
                 this.editMode = false
-                this.$emit('updateCategory',this.index,res.data.category)
-            })
-            .catch(err => {
-                console.log(err.response.data.error)
-            })
+            }
+
+            else if (maxLevel < minLevel) {
+                this.showAlert = true;
+                this.errorMsg = "Error: The minimum level must be either less than or equal to the maximum level set";
+            }
+
+            else {
+                this.$http.put(`${process.env.VUE_APP_URL}/admin/categories/${id}`, {
+                name,minLevel,maxLevel
+                })
+                .then(res => {
+                    this.editMode = false
+                    this.$emit('updateCategory',this.index,res.data.category)
+                })
+                .catch(err => {
+                    this.showAlert = true;
+                    this.errorMsg = err.response ? err.response.data.error : "Error processing request, please try again"
+                })
+            }
             
         }
     }
--- frontend/src/views/admin/Settings.vue
+++ frontend/src/views/admin/Settings.vue
@@ -2,10 +2,65 @@
   <div class="container">
       <h3 class="main-header" style="text-align: center;">Election Settings And Configuration</h3>
       <hr>
-      <div class="row">
-          <div class="col-md-4" v-for="(category,index) in categories" :key="category.id">
-              <SingleCategory :category="category" :levels="levels" :index="index" :updateCategory="updateCategory"/>
+
+        
+        <h4 class="mt-5 text-center">ALL CATEGORIES (POSTS)</h4>
+
+        <v-btn :color="btnColor" fab class="mt-2 btn-fix float-md-right" v-if="!showAddForm" title="Add new category" style="color: floralwhite" @click="showAddForm = true">
+            <v-icon>mdi-plus</v-icon>
+        </v-btn>
+        <v-btn :color="btnColor" fab class="mt-2 btn-fix float-md-right" v-else title="Close form" style="color: floralwhite" @click="showAddForm = false">
+            <v-icon>mdi-close</v-icon>
+        </v-btn>
+      <hr>
+
+    <div class="col-md-8 mx-auto" v-show="showAddForm">
+        <h5>ADD NEW POST</h5>
+        <b-alert class="col-md-12" v-model="showAlert" variant="warning" dismissible>
+            <strong>{{errorMsg}}</strong>
+        </b-alert>
+        <b-form class="col-md-6 mx-auto text-left" @submit.prevent="addCategory()">
+            <b-form-group label-for="name">
+                <template v-slot:label>
+                    Post Name <span class="text-danger">*</span>
+                </template>
+                <b-form-input id="name" v-model="name" required placeholder="e.g Vice President"></b-form-input>
+            </b-form-group>
+
+            <b-form-group label-for="minLevel">
+                <template v-slot:label>
+                    Minimum Level required to run for this post <span class="text-danger">*</span>
+                </template>
+                <b-form-select required v-model="minLevel">
+                    <b-form-select-option :value="level" v-for="level in levels" :key="level">{{level}}</b-form-select-option>
+                </b-form-select>
+            </b-form-group>
+
+             <b-form-group label-for="maxLevel">
+                <template v-slot:label>
+                    Maximum Level required to run for this post <span class="text-danger">*</span>
+                </template>
+                <b-form-select required v-model="maxLevel">
+                    <b-form-select-option :value="level" v-for="level in levels" :key="level">{{level}}</b-form-select-option>
+                </b-form-select>
+            </b-form-group>
+
+            <div class="mt-3">
+                <v-btn type="submit" :loading="loading" :block="true" :color="btnColor" style="color: floralwhite" class="text-capitalize btn btn-block myBtn col-md-6">Submit</v-btn>
+            </div>
+        </b-form>
+        <hr>
+    </div>
+
+      <div class="row" v-if="categories">
+          <div class="col-lg-4" v-for="(category,index) in categories" :key="category.id">
+              <SingleCategory :category="category" :levels="levels" :index="index" v-on:updateCategory="updateCategory" v-on:deleteCategory="deleteCategory"/>
           </div>
+      </div>
+      <div v-else>
+          <p class="text-center">
+              No categories have been created at this time.
+          </p>
       </div>
   </div>
 </template>
@@ -23,7 +78,15 @@ export default {
         return {
             date: "",
             categories: null,
-            levels: []
+            levels: [],
+            name: "",
+            minLevel: ((process.env.VUE_APP_HIGHEST_LEVEL)/(process.env.VUE_APP_HIGHEST_LEVEL)) * 100,
+            maxLevel: process.env.VUE_APP_HIGHEST_LEVEL,
+            showAddForm: false,
+            btnColor: "#162059",
+            loading: false,
+            showAlert: false,
+            errorMsg: "",
         }
     },
     mounted() {
@@ -31,7 +94,7 @@ export default {
     },
     methods: {
         init() {
-            this.$http.get(`${process.env.VUE_APP_URL}/categories`)
+            this.$http.get(`${process.env.VUE_APP_URL}/admin/categories`)
             .then(res => {
                 this.categories = res.data.categories;
                 let highestLevel = process.env.VUE_APP_HIGHEST_LEVEL;
@@ -41,11 +104,57 @@ export default {
                 }
             })
             .catch(err => {
-                console.log(err.response.data.error)
+                console.log(err)
             })
         },
         updateCategory(index,category) {
             this.categories.splice(index,1,category)
+        },
+        deleteCategory(id) {
+            this.$http.delete(`${process.env.VUE_APP_URL}/admin/categories/${id}`)
+            .then(res => {
+                setTimeout(alert(res.data.message), 2500)
+
+                this.categories = this.categories.filter(category => category.id !== id)
+            })
+            .catch(err => {
+                console.log(err.response.data.error)
+            })
+        },
+        addCategory() {
+            let { name, minLevel, maxLevel } = this;
+            this.loading = true;
+
+            //Remove unnecessary white spaces
+            name = name.replace(/\s+/g,' ').trim()
+
+            // Check for some errors before going to the API
+
+            if (maxLevel < minLevel) {
+                this.loading = false;
+                this.showAlert = true;
+                this.errorMsg = "Error: The minimum level must be either less than or equal to the maximum level set";
+            }
+
+            else {
+                this.$http.post(`${process.env.VUE_APP_URL}/admin/categories`, {
+                    name,minLevel,maxLevel
+                })
+                .then(res => {
+                    this.loading = false;
+                    alert(res.data.message)
+                    this.categories.push(res.data.category)
+                    this.name = "";
+                    this.minLevel = ((process.env.VUE_APP_HIGHEST_LEVEL)/(process.env.VUE_APP_HIGHEST_LEVEL)) * 100
+                    this.maxLevel = process.env.VUE_APP_HIGHEST_LEVEL
+                })
+                .catch(err => {
+                    this.loading = false
+                    this.showAlert = true;
+                    this.errorMsg = err.response ? err.response.data.error : "Error processing your request, please try again"
+                })
+            }
+
         }
     }
 }
