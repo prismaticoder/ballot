93a74f72bb128999cf160e9e370ec66a95d8258f
prismathic <pjjsalam1999@gmail.com>
2020-06-01 18:45:22 +0100

    feat: added twitter and instagram urls to single candidate component

--- frontend/src/components/SingleCandidate.vue
+++ frontend/src/components/SingleCandidate.vue
@@ -1,39 +1,27 @@
 <template>
-  <v-card style="text-decoration:none" class="mx-auto" width="20rem" :hover="true" :shaped="true" :to="{name: 'single-candidate', params: {id: candidate.refinedLink}}">
+  <v-card style="text-decoration:none" class="mx-auto mb-5" width="25rem" hover :shaped="true">
 
-    <v-img
-      
-      class="img-fluid"
-      :src="candidate.imageUrl"
-    >
-    </v-img>
+    <v-img class="img-fluid" :src="candidate.imageUrl" :lazy-src="defaultImg" @click="goToCandidate()"></v-img>
 
     <v-card-title>{{candidate.fullName}}</v-card-title>
 
     <v-card-subtitle class="pb-0">{{candidate.level}} Level</v-card-subtitle>
 
-    <!-- <v-card-text class="text--primary">
-      <div>{{candidate.level}} Level</div>
-    </v-card-text> -->
 
     <v-card-actions class="justify-content-center">
-      <v-btn
-        color="orange"
-        text
-      >
-        Share
-      </v-btn>
 
-      <!-- <v-avatar class="m-2" color="indigo" size="36"> -->
-            <!-- <v-img src="../assets/twitter.svg" alt=""></v-img> -->
-        <!-- </v-avatar> -->
+      <v-avatar :color="twitterColor" v-if="candidate.twitter" class="m-3">
+        <v-btn outlined :href="candidate.twitter" class="border-0" target="_blank" style="text-decoration: none">
+          <v-icon style="color:floralwhite">mdi-twitter</v-icon>
+        </v-btn>
+      </v-avatar>
 
-      <v-btn
-        color="orange"
-        text
-      >
-        Explore
-      </v-btn>
+      <v-avatar :color="instagramColor" v-if="candidate.instagram" class="m-3">
+        <v-btn outlined :href="candidate.instagram" class="border-0" target="_blank"  style="text-decoration: none">
+          <v-icon style="color:floralwhite">mdi-instagram</v-icon>
+        </v-btn>
+      </v-avatar>
+
     </v-card-actions>
   </v-card>
 
@@ -42,7 +30,19 @@
 <script>
 export default {
     name: 'SingleCandidate',
-    props: ["candidate"]
+    props: ["candidate"],
+    data() {
+      return {
+        defaultImg: "../assets/logo.svg",
+        twitterColor: "#0072b1",
+        instagramColor: "#7f1734"
+      }
+    },
+    methods: {
+      goToCandidate() {
+        this.$router.push({name: 'single-candidate', params: {id: this.candidate.refinedLink}})
+      }
+    }
 }
 </script>
 
