519c2f65e341826286f133c347cd54eef6c57161
prismathic <pjjsalam1999@gmail.com>
2020-06-03 16:39:56 +0100

    basically done with the voter list page

--- frontend/src/views/admin/Voters.vue
+++ frontend/src/views/admin/Voters.vue
@@ -1,141 +1,102 @@
 <template>
-  <div class="container mt-4">
-    <h4 class="text-center mt-5"><span class="redColor text-uppercase">{{region.name}}</span> VOTERS</h4>
+  <div class="container">
+    <h3 class="main-header" style="text-align: center;">Download Voter List</h3>
+    <hr>
 
-    <form action="" class="voterForm mt-4 mb-5" >
-    <div class="input-group mb-3 col-md-5 mx-auto">
-            <input type="search" v-model="matric" id="voterText" class="form-control" placeholder="Search Voters By Matric..." aria-label="Enter Matric Number" aria-describedby="basic-addon2">
-            <div class="input-group-append">
-            <button @click.prevent="getMatric(matric)" class="btn myBtn voterBtn" type="submit"><i class="fa fa-search" style="color: #BF1111;"></i></button>
-            </div>
-    </div>
-    </form>
-
-    <table class="table mt-1 table-sm table-bordered">
-        <thead class="blueColor">
-            <tr>
-                <th>S/N</th>
-                <th>Matric</th>
-                <th>Firstname</th>
-                <th>Surname</th>
-                <th v-if="region.type == 'hall'">Faculty</th>
-                <th v-if="region.type == 'hall'">Department</th>
-                <th v-else-if="region.type == 'faculty'">Department</th>
-                <th>Level</th>
-            </tr>
-        </thead>
-        <tbody id="tableBody">
-                <tr v-bind:key="voter.id" v-for="(voter, index) in voters">
-                    <!-- The "10" here is dependent on what is in the perPage attribute -->
-                    <td>{{(10 * (currentPage - 1)) + index+1}}</td>
-                    <td>{{ voter.matric }}</td>
-                    <td class="text-capitalize">{{ voter.firstName.toLowerCase() }}</td>
-                    <td class="text-capitalize">{{ voter.lastName.toLowerCase() }}</td>
-                    <td v-if="region.type == 'hall'">{{ voter.faculty }}</td>
-                    <td v-if="region.type == 'hall'">{{ voter.department }}</td>
-                    <td v-else-if="region.type == 'faculty'">{{ voter.department }}</td>
-                    <td>{{ voter.level }}</td>
-                </tr>
-        </tbody>
-        
-    </table>
-
-
-    <nav v-show="!showVisible" class="mt-4" v-if="pages > 1" aria-label="Page navigation">
-        <ul class="pagination justify-content-center">
-            <li v-if="currentPage == 1" class="page-item disabled">
-                <span class="page-link" tabindex="-1">Previous</span>
-            </li>
-            <li class="page-item" v-else>
-                <a href="#" class="page-link" @click.prevent="getPage(parseInt(currentPage) - 1)">Previous</a>
-            </li>
-
-            <li v-bind:key="page" v-bind:class="{'page-item':true, 'active':(page == currentPage)}" v-for="page in pages">
-                <a href="#" v-if="page == currentPage" @click.prevent="getPage(page)" class="page-link active">{{page}}</a>
-                <a href="#" v-else @click.prevent="getPage(page)" class="page-link">{{page}}</a>
-            </li>
-
-           <li v-if="currentPage == pages" class="page-item disabled">
-                <a href="" class="page-link" tabindex="-1">Next</a>
-            </li>
-            <li class="page-item" v-else>
-                <a href="#" class="page-link" tabindex="-1" @click.prevent="getPage(parseInt(currentPage) + 1)">Next</a>
-            </li>
+    <div class="container mt-5">
+        <ul class="text-left list-group-flush">
+            <a href="#" class="list-group-item mt-1 blueColor" @click.prevent="downloadAll()">All Eligible Voters (PDF) <v-icon>mdi-download</v-icon> </a>
+            <a href="#" class="list-group-item mt-1 blueColor" @click.prevent="downloadAccredited()">All Accredited Voters (PDF) <v-icon>mdi-download</v-icon> </a>
         </ul>
-    </nav>
-    <div class="col-md-8 mx-auto text-center" v-if="showVisible">
-        <a @click.prevent="init" class="backBtn specColor" href="">Back to Main List</a>
     </div>
 </div>
 </template>
 
 <script>
 
-import axios from 'axios';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 
 export default {
     name: "Voters",
     data() {
         return {
-            voters: [],
-            region: {},
-            matric: '',
-            pages: '',
-            currentPage: null,
-            nextPage: '',
-            showVisible: false
+            type: process.env.VUE_APP_TYPE.toLowerCase(),
+            name: process.env.VUE_APP_NAME
         }
     },
     methods: {
-        init() {
-            axios.get('http://localhost:4010/law/admin/voters?perPage=10')
+        downloadAll() {
+            this.$http.get('admin/voters')
             .then(res => {
-                if (res.data.ok) {
-                    this.voters = res.data.voters;
-                    this.region = res.data.region;
-                    this.pages = res.data.totalPages;
-                    this.nextPage = res.data.nextPage;
-                    this.currentPage = res.data.currentPage
-                    this.showVisible = false;
+                if (res.data.voters.length > 0) {
+                    let voters = res.data.voters.map((voter,index) => {
+                    return [index+1 , voter.matric, voter.fullName, voter.level, voter.department]
+                    })
+
+                    var doc = new jsPDF();
+                    doc.autoTable({
+                        head: [['S/N', 'MATRIC. NO', 'NAME', 'LEVEL', 'DEPARTMENT']],
+                        body: voters,
+                    })
+
+                    let name = this.name.toLowerCase().split(' ').join('-')
+                    name = name + '-voter-list.pdf'
+
+                    doc.save(name)
                 }
-            })
-            .catch(err => console.log(err))
-        },
-        getMatric(matric) {
-            axios.get(`http://localhost:4010/law/admin/voters/search?q=${matric}`)
-            .then(res => {
-                if (res.data.ok) {
-                    this.voters = [res.data.voter];
-                    this.showVisible = true;
-                }
+
                 else {
-                    alert (res.data.message)
+                    alert ("There is currently no voter list")
                 }
+                
+
             })
-            .catch(err => console.log(err))
+            .catch(err => {
+                console.log(err.response)
+            })
         },
-        getPage(page) {
-            axios.get(`http://localhost:4010/law/admin/voters?page=${page}&perPage=10`)
+        downloadAccredited() {
+
+            this.$http.get('admin/voters?type=accredited')
             .then(res => {
-                if (res.data.ok) {
-                    this.voters = res.data.voters;
-                    this.region = res.data.region;
-                    this.pages = res.data.totalPages;
-                    this.currentPage = res.data.currentPage
-                    this.nextPage = res.data.nextPage;
-                    this.showVisible = false;
+
+                if (res.data.voters.length > 0) {
+                    let voters = res.data.voters.map((voter,index) => {
+                    return [index+1 , voter.matric, voter.fullName, voter.level, voter.department]
+                    })
+                    
+                    var doc = new jsPDF();
+
+                    doc.autoTable({
+                        head: [['S/N', 'MATRIC. NO', 'NAME', 'LEVEL', 'DEPARTMENT']],
+                        body: voters,
+                    })
+
+                    let name = this.name.toLowerCase().split(' ').join('-')
+                    name = name + '-accredited-voter-list.pdf'
+
+                    doc.save(name)
                 }
+
+                else {
+                    alert ("There is currently no accredited voter list")
+                }
+
             })
-            .catch(err => console.log(err))
-        }
-    },
-    mounted() {
-        this.init()
+            .catch(err => {
+                console.log(err.response)
+            })
+        },
     }
 }
 </script>
 
-<style>
+<style scoped>
+
+    a {
+        text-decoration: none;
+    }
 
 </style>
