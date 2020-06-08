eb262a17a7747b26659ca6baee9a75db77851955
prismathic <pjjsalam1999@gmail.com>
2020-06-08 23:38:24 +0100

    lazy loading for stats page

--- frontend/src/views/Stats.vue
+++ frontend/src/views/Stats.vue
@@ -3,7 +3,7 @@
       <h3 class="main-header" style="text-align: center;">Voting Stats By Level</h3>
       <hr>
 
-      <div class="row mt-5 justify-content-center">
+      <div class="row mt-5 justify-content-center" v-if="isLoaded">
         <div class="col-lg-3 col-4 m-3 border" v-for="stat in stats" :key="stat.level">
             <div class="col-12">
                 <h5>{{stat.level}} LEVEL</h5>
@@ -12,6 +12,14 @@
                 <span class="timer">{{stat.count}}</span>
             </div>
         </div>
+      </div>
+      <div class="row mt-5 justify-content-center" v-else> 
+        <v-skeleton-loader type="card" class="m-3 col-lg-3 col-4"></v-skeleton-loader>
+        <v-skeleton-loader type="card" class="m-3 col-lg-3 col-4"></v-skeleton-loader>
+        <v-skeleton-loader type="card" class="m-3 col-lg-3 col-4"></v-skeleton-loader>
+        <v-skeleton-loader type="card" class="m-3 col-lg-3 col-4"></v-skeleton-loader>
+        <v-skeleton-loader type="card" class="m-3 col-lg-3 col-4"></v-skeleton-loader>
+        <v-skeleton-loader type="card" class="m-3 col-lg-3 col-4"></v-skeleton-loader>
       </div>
       
   </div>
@@ -22,7 +30,8 @@ export default {
     name: "Stats",
     data() {
         return {
-            stats: null
+            stats: null,
+            isLoaded: false
         }
     },
     methods: {
@@ -30,6 +39,7 @@ export default {
             this.$http.get('stats')
             .then(res => {
                 this.stats = res.data.stats
+                this.isLoaded = true
             })
             .catch(err => alert(err.response ? err.response.data.error : "Error processing request"))
         }
