35dfd49a738b9a2a66bdebe0518384d59f0bf5a5
prismathic <pjjsalam1999@gmail.com>
2020-06-08 23:38:48 +0100

    feat to wait for approval before showing election results

--- frontend/src/views/Result.vue
+++ frontend/src/views/Result.vue
@@ -2,12 +2,31 @@
   <div>
     <h3 class="main-header" style="text-align: center;">Approved Election Results</h3>
     <hr>
+    <div v-if="isLoaded && !pageText">
+        <div class="mt-5 mx-auto" :id="category.id" v-for="category in categories" v-bind:key="category.id">
+            <h4 class="text-center">CATEGORY: {{category.name.toUpperCase()}}</h4>
+            <hr>
+            <CategoryResult class="justify-content-center mt-4" :category="category" :totalVotes="totalVotes"/>
+        </div>
+    </div>
+    <div v-else-if="isLoaded && pageText">
+        <div class="mt-5 mx-auto">
+            <p>
+                {{pageText}}
+            </p>
+        </div>
+    </div>
+    <div v-else>
+        <v-skeleton-loader class="mx-auto mt-5" type="heading"></v-skeleton-loader>
 
-    
-    <div class="mt-5 mx-auto" :id="category.id" v-for="category in categories" v-bind:key="category.id">
-        <h4 class="text-center">CATEGORY: {{category.name.toUpperCase()}}</h4>
-        <hr>
-        <CategoryResult class="justify-content-center mt-4" :category="category" :totalVotes="totalVotes"/>
+        <div class="row justify-content-center">
+            <v-skeleton-loader type="card, text" class="m-3" width="20rem"></v-skeleton-loader>
+            <v-skeleton-loader type="card, text" class="m-3" width="20rem"></v-skeleton-loader>
+            <v-skeleton-loader type="card, text" class="m-3" width="20rem"></v-skeleton-loader>
+            <v-skeleton-loader type="card, text" class="m-3" width="20rem"></v-skeleton-loader>
+            <v-skeleton-loader type="card, text" class="m-3" width="20rem"></v-skeleton-loader>
+            <v-skeleton-loader type="card, text" class="m-3" width="20rem"></v-skeleton-loader>
+        </div>
     </div>
 
   </div>
@@ -27,7 +46,8 @@ export default {
         return {
             isLoaded: false,
             categories: null,
-            totalVotes: null
+            totalVotes: null,
+            pageText: null
         }
     },
     methods: {
@@ -39,7 +59,8 @@ export default {
                 this.isLoaded = true
             })
             .catch(err => {
-                alert(err.response ? err.response.data.error : "Error processing request, please try again")
+                this.pageText = err.response ? err.response.data.error : "Error processing request, please try again"
+                this.isLoaded = true;
             })
         }
     },
